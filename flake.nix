13c0be2882bdab8aee8af63d30ca5a3d5cece220
Mikael Voss <mvs@nyantec.com>
2024-08-07 15:19:08 +0200

    Use nix-community cache as extra substituter

--- flake.nix
+++ flake.nix
@@ -24,6 +24,11 @@
 
   nixConfig = {
     allow-import-form-derivation = true;
+
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
